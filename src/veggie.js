284a54ea498edf9839a4953b380a18a91ddc3228
Taisia Makhmutova <taia.makh@gmail.com>
2021-04-07 22:42:03 +0200

    ADD slice parabolic effect

--- src/veggie.js
+++ src/veggie.js
@@ -22,14 +22,10 @@ class Veggie {
     }else{
       const slicedImg = document.createElement('img');
       slicedImg.src = `../images/fruit-cutter-game-kit/PNG/fruits/slicedFruits/sliced${this.fruit}.png`
-
-      let numPlus = 
-      let numMenus ?= 
-
-      this.ctx.drawImage(slicedImg, this.x-10, this.y, this.size, this.size);
-
-      this.ctx.drawImage(entireImg, this.x+10, this.y, this.size, this.size);
-
+    
+      let quadratic = Math.sqrt(this.y/0.05)
+      this.ctx.drawImage(slicedImg, 60+this.x-quadratic, this.y, this.size, this.size);
+      this.ctx.drawImage(entireImg, -60+this.x+quadratic, this.y, this.size, this.size);
       
     }
   }
